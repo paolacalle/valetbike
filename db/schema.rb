f0dd917dac04b7719a629e7b2f1e6dab23efea8c
chel48 <cfowler65@smith.edu>
2023-11-05 17:57:44 -0500

    payment amount forgotten column

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_05_190001) do
 
   create_table "payments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "credit_card_info"
+    t.float "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
