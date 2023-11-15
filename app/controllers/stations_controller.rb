657c5914ca4216a17d1e807e0947d927745b47f7
Paola Calle <pcalle@smith.edu>
2023-11-15 14:36:37 -0500

    added search button and added some css

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -2,6 +2,11 @@ class StationsController < ApplicationController
   
   def index
     @stations = Station.all.order(identifier: :asc)
+    
+    if params[:search_by_address] && params[:search_by_address] != ""
+      @stations = @stations.where("address LIKE ?", "%#{params[:search_by_address]}%")
+    end
+    puts @stations
   end
   
 end
