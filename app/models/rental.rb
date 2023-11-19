e2e120ccff317cd7331e189560b6d285463fce90
kemerson-ryan <kemerson@smith.edu>
2023-11-19 12:55:20 -0500

    rental flashes worknow success and error displayed

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,5 +1,5 @@
 class Rental < ApplicationRecord
     validates_presence_of   :rental_period,
                             :return_by
-    belongs_to :user, class_name: :User, foreign_key: :user
+    belongs_to :user, class_name: :User, foreign_key: :id, optional:true
 end
