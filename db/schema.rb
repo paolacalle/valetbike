806e759aeef2946b6e7c7d3ec3de1f668233a652
kemerson-ryan <kemerson@smith.edu>
2023-11-19 12:12:08 -0500

    Flashes will now display errors and successes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_11_041347) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_15_165520) do
   create_table "bikes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "identifier"
     t.integer "current_station_id"
@@ -47,6 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_11_041347) do
     t.datetime "return_by"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user"
   end
 
   create_table "stations", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
