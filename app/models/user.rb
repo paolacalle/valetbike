1fa672968ada5e02e175f5e994206882bfe69225
Paola Calle <pcalle@smith.edu>
2023-12-09 21:31:08 -0500

    pin verfication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+    # Additional columns for email verification
+    attr_accessor :pin_0, :pin_1, :pin_2, :pin_3
+
     # Include default devise modules. Others available are:
     # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
     devise :database_authenticatable, :registerable,
@@ -16,4 +19,35 @@ class User < ApplicationRecord
     has_many :rentals, class_name: :Rental, foreign_key: :id
 
     # validates :email, format: {with: EMAIL_REGEX, message: "Email invalid" }
+
+    # Email Verification Callbacks
+    after_create :update_user_verified_column_to_true
+    after_create :send_pin!
+
+    # Method to update the user's email verification status to true
+    def update_user_verified_column_to_true
+        # Perform the email verification process after user creation
+        UpdateUserJob.perform_now(self)
+    end
+
+    # Method to reset the PIN for email verification
+    def reset_pin!
+        update_column(:pin, rand(1000..9999))
+    end
+
+    # Method to unverify the user's email
+    # sets the email verification status to "false" to allow re-verification.
+    def unverify!
+        update_column(:verified, false)
+    end
+
+    # Method to send the PIN for email verification
+    # sets the verification status to "false," 
+    # triggers a background job to send the PIN to the user.
+    def send_pin!
+        reset_pin!
+        unverify!
+        # Perform the job to send the PIN
+        SendPinJob.perform_now(self)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_08_003506) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_10_020225) do
   create_table "bikes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "identifier"
     t.integer "current_station_id"
@@ -86,6 +86,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_08_003506) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "pin"
+    t.datetime "pin_sent_at"
+    t.boolean "verified", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
