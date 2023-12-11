cdb41fb2f1942088e2299a33c21694adacb4592e
Paola Calle <pcalle@smith.edu>
2023-12-11 11:39:38 -0500

    emails are confirmable and lockable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_10_084008) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_11_163339) do
   create_table "bikes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "identifier"
     t.integer "current_station_id"
@@ -64,7 +64,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_10_084008) do
     t.boolean "is_complete"
     t.integer "bike_id"
     t.boolean "payment_required", default: false
-    t.decimal "payment_amount", precision: 10, scale: 2
+    t.decimal "payment_amount", precision: 10, scale: 2, default: "0.0"
   end
 
   create_table "stations", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -92,11 +92,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_10_084008) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer "pin"
-    t.datetime "pin_sent_at"
-    t.boolean "verified", default: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
 end
