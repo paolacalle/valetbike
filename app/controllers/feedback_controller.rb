0aeb6fbf46da8e5c945fea7518a24b56c38ea9f6
chel48 <cfowler65@smith.edu>
2023-12-11 03:46:05 -0500

    start on getting a confirmation email to send after submitting feedback, from controller?

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -1,3 +1,5 @@
+
+
 class FeedbackController < ApplicationController
 
   def new
@@ -20,6 +22,8 @@ class FeedbackController < ApplicationController
   def save_feedback
     @feedback.user_id = current_user.id
     if @feedback.save
+      puts "saved"
+      send_email
       flash[:success] = "Response submitted. Thank you for your feedback!"
       redirect_to home_path
     end
@@ -29,6 +33,29 @@ class FeedbackController < ApplicationController
     params.permit(:response)
   end
 
-  def mail
+  
+
+  def send_email
+    puts "entered send email"
+    from = SendGrid::Email.new(email: 'bikeruby4@gmail.com')
+    puts "from made"
+    to = SendGrid::Email.new(email: @user.email)
+    puts "to made"
+    subject = 'Sending with Twilio SendGrid is Fun'
+    puts "subject made"
+    content = SendGrid::Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
+    puts "content made"
+    mail = SendGrid::Mail.new(from, subject, to, content)
+    puts "formed"
+
+    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+    puts "sg done"
+    response = sg.client.mail._('send').post(request_body: mail.to_json)
+    puts response.status_code
+    puts response.body
+    puts response.parsed_body
+    puts response.headers
+
+
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_10_065254) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_10_084008) do
   create_table "bikes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "identifier"
     t.integer "current_station_id"
@@ -71,7 +71,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_10_065254) do
     t.boolean "is_complete"
     t.integer "bike_id"
     t.boolean "payment_required", default: false
-    t.decimal "payment_amount", precision: 10, scale: 2
+    t.decimal "payment_amount", precision: 10, scale: 2, default: "0.0"
   end
 
   create_table "stations", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -93,12 +93,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_10_065254) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "current_rental"
-    t.boolean "has_membership"
-    t.boolean "has_payment"
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.boolean "has_membership"
+    t.boolean "has_payment"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
