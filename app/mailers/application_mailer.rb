3811350a6daa4585f9ff541951035f4686101477
Paola Calle <pcalle@smith.edu>
2023-12-11 20:29:42 -0500

    small changes

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,8 @@
 class ApplicationMailer < ActionMailer::Base
+  default from: 'bikeruby4@gmail.com'
+
   def notify_company_of_feedback(feedback_content)
     mail(to: 'bikeruby4@gmail.com', subject: 'NEW Feedback Posted', body: "The Feedback:\n#{feedback_content}")
   end
+  
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,4 @@ class UserMailer < ApplicationMailer
     mail(to: user_email, subject: 'Valetbike Feedback Confirmation', body: 'Thank you. Your feedback was sent to bikeruby4@gmail.com for further review.')
   end
 
-  def notify_company_of_feedback(feedback_content)
-    mail(to: 'bikeruby4@gmail.com', subject: 'NEW Feedback Posted', body: "The Feedback:\n#{feedback_content}")
-  end
 end
