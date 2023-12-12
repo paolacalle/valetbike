ef2bf4227f57fa35fbac5a2805adf0ed787cf77f
chel48 <cfowler65@smith.edu>
2023-12-12 00:44:32 -0500

    change background color of past rental hovers, adjustments to nav bar and footer, add back arrow to qr code page

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,4 +1,4 @@
 class Feedback < ApplicationRecord
-    # Ensure message is present and has more than 20 characters
-    validates :response, presence: true, length: { minimum: 20 }
+    # Ensure message is present and has more than 10 characters
+    validates :response, presence: true, length: { minimum: 10 }
 end
