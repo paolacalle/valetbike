39c1d5c4ef7384ba595d4ee128a92e8fbeba45b9
Paola Calle <pcalle@smith.edu>
2023-12-12 11:02:43 -0500

    minor changes, quick fix to nav

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,6 +1,6 @@
 ActionMailer::Base.smtp_settings = {
   user_name: "apikey", # do not change this field
-  password: ENV["SENDGRID_API_KEY"], 
+  password:  ENV["SENDGRID_API_KEY"], 
   address: "smtp.sendgrid.net",
   port: 587,
   authentication: :plain,
