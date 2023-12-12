544a401c642cf4fc8ccd98d80237eb247220fce0
Paola Calle <pcalle@smith.edu>
2023-12-11 20:20:29 -0500

    deleted dead code

--- app/javascript/controllers/confirmation_controller.js
+++ app/javascript/controllers/confirmation_controller.js
@@ -1,16 +1,17 @@
 import { Controller } from "@hotwired/stimulus"
+// Makes it harder for user to cancel account 
 
 // Connects to data-controller="confirmation"
 export default class extends Controller {
   confirm(event) {
     event.preventDefault();
 
-    if (!confirm('Are you absolutely sure you want to cancel your account? This action cannot be undone.')) {
+    if (!confirm('Are you 100% sure you want to cancel your account? This action cannot be undone!')) {
       return;
     }
 
-    var userInput = prompt('Please type "CANCEL" to confirm account deletion.');
-    if (userInput === 'CANCEL') {
+    var userInput = prompt('Please type "BYE VALETBIKE" to confirm account deletion.');
+    if (userInput === 'BYE VALETBIKE') {
       event.target.closest('form').submit();
     } else {
       alert('Account deletion canceled.');
