7d0e2ffacd24785ce6d377e2564147c20e2cf83e
chel48 <cfowler65@smith.edu>
2023-12-11 22:34:23 -0500

    feedback and review forms working with email confirmation

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -9,6 +9,11 @@ class FeedbackController < ApplicationController
 
   def create
     logger.info("entered create")
+    puts params[:response]
+    @response = params[:response]
+    @user = User.find(current_user.id)
+    user_id = @user
+    response = params[:response]
     @feedback = Feedback.new(feedback_params)
     save_feedback
   end
@@ -16,8 +21,7 @@ class FeedbackController < ApplicationController
   def save_feedback
     @feedback.user_id = current_user.id
     if @feedback.save
-      UserMailer.send_feedback_confirmation(current_user.email).deliver_now
-      ApplicationMailer.notify_company_of_feedback(@feedback.response).deliver_now
+      send_email
       flash[:success] = "Response submitted. Thank you for your feedback!"
       redirect_to home_path
     else 
@@ -30,6 +34,24 @@ class FeedbackController < ApplicationController
     params.permit(:response)
   end
 
-  
+  def send_email
+    logger.info("entered send email")
+    from = SendGrid::Email.new(email: 'bikeruby4@gmail.com')
+    to = SendGrid::Email.new(email: @user.email)
+    subject = 'ValetBike Feedback Received'
+    text = "Thank you for submitting feedback to ValetBike! Your feedback has been received. Below is a copy of your response.\n\n" + "\"" + @response + "\""
+    content = SendGrid::Content.new(type: 'text/plain', value: text)
+    mail = SendGrid::Mail.new(from, subject, to, content)
+ 
+ 
+    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+    response = sg.client.mail._('send').post(request_body: mail.to_json)
+    puts response.status_code
+    puts response.body
+    puts response.headers
+ 
+ 
+  end
+ 
 
 end
